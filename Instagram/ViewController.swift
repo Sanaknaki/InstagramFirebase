2a4d8cf9af203eb02939f3394167095843acd4ea
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-03-25 14:56:41 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ali Sanaknaki on 2020-03-25.
+//  Copyright © 2020 Ali Sanaknaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
